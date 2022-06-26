670f85e5454d0d8e53aa2b5190616ac45f5c6803
yomabd <idreesyomi@gmail.com>
2022-06-26 13:15:44 +0100

    register Post model

--- I4G003513VED/blog/admin.py
+++ I4G003513VED/blog/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from .models import Post
 
 # Register your models here.
 
 admin.site.register(Post)
-=======
 
-# Register your models here.
->>>>>>> fdfdeae (initial project)
