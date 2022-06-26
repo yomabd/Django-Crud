b08f8efe79829abb3e4c9b26377ab40b691b8894
yomabd <idreesyomi@gmail.com>
2022-06-26 12:40:52 +0100

    complete project

---
+++ I4G003513VED/blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+<<<<<<< HEAD
+from .models import Post
+
+# Register your models here.
+
+admin.site.register(Post)
+=======
+
+# Register your models here.
+>>>>>>> fdfdeae (initial project)
---
+++ I4G003513VED/blog/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+# Create your views here.
+
+## postListView class definition
+class PostListView(ListView):
+    model= Post
+
+#postCreateView class definition
+class PostCreateaview(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+#postDetailView class definition
+class PostDetailView(DetailView):
+    model = Post
+
+#postUpdateView class definition
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+#postDeleteView class definition
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
