be7ca8985af4b9e029ab0ed0b84ead76c285d8a7
yomabd <idreesyomi@gmail.com>
2022-06-26 13:22:00 +0100

    install blog app

--- I4G003513VED/I4G003513VED/settings.py
+++ I4G003513VED/I4G003513VED/settings.py
@@ -37,10 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'blog',
-=======
->>>>>>> fdfdeae (initial project)
+
 ]
 
 MIDDLEWARE = [
